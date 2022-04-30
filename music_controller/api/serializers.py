ec95560538798b1f5fb207fb53cb2827fe1fde09
Bennett <banorman@umich.edu>
2022-04-30 17:26:55 -0400

    setting up posts requests

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Room
 
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        # Serialize the request and make sure it is all valid
+        fields = ('guest_can_pause', 'votes_to_skip')
--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import RoomView
+from .views import CreateRoomView
 
 urlpatterns = [
-    path('room', RoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('view-room', RoomView.as_view())
 ]
